7de7c88a0af93d8bcd31d2d48236ced1195e2f9c
breannams <breannamarie_s@hotmail.com>
2021-03-22 20:35:20 -0400

    Rollback migrations to redo npc table easier.

--- db/migrate/20210322214635_create_npcs.rb
+++ db/migrate/20210322214635_create_npcs.rb
@@ -5,16 +5,23 @@ class CreateNpcs < ActiveRecord::Migration[6.1]
       t.string :name
       t.integer :cr_rating
       t.integer :level
+      t.integer :exp
       t.string :species
       t.string :class
       t.integer :hp
       t.integer :eac
       t.integer :kac
-      t.integer :saving_throws
+      t.integer :fort_save
+      t.integer :ref_save
+      t.integer :will_save
       t.integer :speed
-      t.integer :ability_scores
-      t.integer :skills
+      t.string :master_skill
+      t.string :good_skill
+      t.string :higher_ability_mod
+      t.string :lower_ability_mod
       t.string :special_abilities
+      t.string :optional_info
+      t.belongs_to :user
     end
   end
 end
--- db/migrate/20210323000707_add_userid_to_npcs.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToNpcs < ActiveRecord::Migration[6.1]
-  def change
-      add_column :npc, :user_id, :integer
-  end
-end
