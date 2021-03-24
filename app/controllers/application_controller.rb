232c68edfdd06ebd47e3a33f1971bce9da873c89
breannams <breannamarie_s@hotmail.com>
2021-03-23 20:52:53 -0400

    added a healper method file to keep separate.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,4 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
-
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
 end
