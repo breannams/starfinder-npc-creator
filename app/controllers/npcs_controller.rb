88396813b959d7634fe56e0ffda72c622e84e476
breannams <breannamarie_s@hotmail.com>
2021-03-31 18:21:58 -0400

    Add edit code to npcs controller.

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -36,8 +36,8 @@ class NpcController < ApplicationController
     end
 
     get '/npcs/:id/edit' do
-        if logged_in?
-            @npc = Npc.find_by_id(param[:id])
+        @npc = Npc.find_by_id(param[:id])
+        if logged_in? && current_user == @npc.user
         erb :'npcs/edit'
         else
             redirect to '/'
@@ -45,16 +45,46 @@ class NpcController < ApplicationController
     end
 
     patch '/npcs/:id' do
-
-    end
+        @npc = Npc.find_by_id(params[:id]
+        
+        if logged_in?
+            @npc.name = params[:name]
+            @npc.cr_rating = params[:cr_rating]
+            @npc.level = params[:level]
+            @npc.exp = params[:exp]
+            @npc.species = params[:species]
+            @npc.class = params[:class]
+            @npc.hp = params[:hp]
+            @npc.eac = params[:eac]
+            @npc.kac = params[:kac]
+            @npc.fort_save = params[:fort_save]
+            @npc.ref_save = params[:ref_save]
+            @npc.will_save = params[:will_save]
+            @npc.speed = params[:speed]
+            @npc.master_skill = params[:]
+            @npc.good_skill = params[:]
+            # @npc.ability_mod1 = params[:]
+            # @npc.ability_mod2 = params[:]
+            # @npc.ability_mod3 = params[:]
+            #@npc.offense_ability
+            #@npc.defense_ability
+            #@npc.immunity
+            #@npc.melee
+            #@npc.ranged
+            @npc.special_abilities
+            @npc.optional_info
+            @npc.save
+            redirect to '/npcs/#{@npc.id}'
+        else
+            redirect to '/'
+    
+     end
 
     delete '/npcs/:id/delete' do
-        @npc = current_user.npc.find_by(id: params[:id])
-        if @npc && @npc.delete
-            redirect to '/npcs'
-        else
-            redirect to 'npcs/#{@npc.id}'
-        end
+        @npc = find_by_id(params[:id])
+        
+        redirect to '/npcs'
+        
     end
 
 end
